bc8691570a0269fd8e0cb53b1a67b821b78e2c74
Bojan Baltic <bojan.baltic@gmail.com>
2024-07-04 15:09:23 +0200

    Add some print statements while writing to s3

--- ai-db-writer/L-CAM/L_CAM_VGG16/dicom_to_png.py
+++ ai-db-writer/L-CAM/L_CAM_VGG16/dicom_to_png.py
@@ -95,12 +95,15 @@ def insert_dicom_metadata(table_name, mammography_id, patient_name, patient_id, 
 
 def write_oracle_s3(bucket_name, png_filepath, png_image):
     # TODO: Add check if file is already uploaded
+    print(f"Saving to oracle s3: {png_filepath}")
     object_storage_client.put_object(
         namespace_name=oci_namespace,
         bucket_name=bucket_name,
         object_name=png_filepath,
         put_object_body=png_image
     )
+    print(f"Saved to oracle s3: {png_filepath}")
+    print(object_storage_client.__dict__)
 
 def write_minio(bucket_name, png_filepath, png_image):
         # Save to minio
