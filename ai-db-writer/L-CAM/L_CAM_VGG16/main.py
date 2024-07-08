bf94c48a4c6364fb65cb11bab05a5cc97c013062
Bojan Baltic <bojan.baltic@gmail.com>
2024-07-08 12:15:47 +0200

    Add print statements and checks for file save

--- ai-db-writer/L-CAM/L_CAM_VGG16/main.py
+++ ai-db-writer/L-CAM/L_CAM_VGG16/main.py
@@ -33,7 +33,21 @@ def write_postgres(engine, study_uid, model_1_result):
 def write_heatmap(heatmap, study_uid, tmp_heatmap_folder):
     png_image = f'{study_uid}.png'
     png_filepath = f'{tmp_heatmap_folder}/{png_image}'
-    cv2.imwrite(png_filepath, heatmap)
+
+    directory = os.path.dirname(png_filepath)
+    print(f"Directory is: {directory}")
+    if not os.path.exists(directory) and directory != '':
+        print(f"Directory does not exist: {directory}")
+    else:
+        print(f"Directory exists: {directory}")
+    # Check if the file path is writable
+    if os.access(directory, os.W_OK):
+        print(f"Directory is writable: {directory}")
+    else:
+        print(f"Directory is not writable: {directory}")
+
+    heatmap_image_saved = cv2.imwrite(png_filepath, heatmap)
+    print(f"Image saved: {heatmap_image_saved}")
     write_minio('heatmaps', png_filepath, png_image)
     write_oracle_s3('bucket-aimambo-heatmaps', png_filepath, heatmap)
 
@@ -45,14 +59,12 @@ def main():
     print("Start main in ai-db-writter")
     engine = get_db_engine()
     print("Set engine finished")
-    """
     for img_path, img in inference_loader(dicom_folder, 1):
         print(f"ai-db-writter: {img_path}")
         study_uid = img_path[0].split(os.path.sep)[-1]
         model_1_result, heatmap = process_img(img)
         write_postgres(engine, study_uid, model_1_result)
         write_heatmap(heatmap, study_uid, tmp_heatmap_folder)
-    """
     png_to_minio(dicom_folder, tmp_png_folder)
 
 if __name__ == '__main__':
